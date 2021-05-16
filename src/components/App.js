c8c9d0be1194957d15b0cb453e050e062b6b5f14
Tvsmadhur <67414114+Tvsmadhur@users.noreply.github.com>
2021-05-16 16:03:19 +0530

    Create App.js

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React,{Components} from 'react;
+import Shape from'./Shape';
+import Circle from './Circle';
+
+class Circle extends React.Components
+{
+  render()
+  {
+    return 
+    (
+      {
+        <Circle/>
+        <Shape/>
+      }
+     )
+  }
+}
